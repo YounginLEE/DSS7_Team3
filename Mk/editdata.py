7a3ff52dfd1ad9d1aa519c8f6222ec559c01a251
Moons08 <mskk0805@gmail.com>
2018-04-07 08:11:01 +0900

    fix editdata

--- Mk/editdata.py
+++ Mk/editdata.py
@@ -2,77 +2,53 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import *
 
-def weekday(x):
-    if x == 'Monday': return 1
-    elif x == 'Tuesday' : return 2
-    elif x == 'Wednesday' : return 3
-    elif x == 'Thursday' : return 4
-    elif x == 'Friday' : return 5
-    elif x == 'Saturday' : return 6
-    elif x == 'Sunday' : return 7
+def FN_dummies(df, test): # 메모리 문제로 돌리지를 못함
+    both = set(test['FinelineNumber'].unique()) & set(df['FinelineNumber'].unique())
+    df = df[df['FinelineNumber'].isin(both)] # train과 test 둘다 있는 것만으로 모델링
 
-def FN(df, test): # 메모리 문제로 돌리지를 못함
-    df.dropna(how='all', inplace=True, subset=['FinelineNumber'])
+    df['FinelineNumber']=df['FinelineNumber'].astype(str) # get_dummies 조건
+    test['FinelineNumber']=test['FinelineNumber'].astype(str)
 
-    # df['FinelineNumber'].fillna(0, inplace=True)
-    # test['FinelineNumber'].fillna(0, inplace=True)
-    df['FinelineNumber'].dropna(how='any', inplace=True)
-    test['FinelineNumber'].dropna(how='any', inplace=True)
+    FN = pd.get_dummies(df[['VisitNumber','FinelineNumber']]).groupby('VisitNumber').sum()
+    T_FN = pd.get_dummies(df[['VisitNumber','FinelineNumber']]).groupby('VisitNumber').sum()
 
-    df = df[df['FinelineNumber'].isin(a&b)] # train과 test 둘다 있는 것만으로 모델링
-    df.reset_index(drop=True, inplace=True)
-    a = set(test['FinelineNumber'].unique())
-    b = set(df['FinelineNumber'].unique())
+    not_in_train = list(set(T_FN.columns) - set(FN.columns))
+    if not_in_train:
+        T_FN.drop(not_in_train, axis=1, inplace=True) # 0으로 설정된 컬럼 삭제 (train에 없음)
 
-    index = test[test['FinelineNumber'].isin(a-b)].index
-    test['FinelineNumber'].loc[index] = 0 # train에 없는 애들은 0 으로 설정
+    FN.reset_index(inplace=True)
+    T_FN.reset_index(inplace=True)
 
-    lb = LabelBinarizer() # 객체 생성
-    D_FL = lb.fit_transform(df['FinelineNumber'].values.tolist())
-    T_FL = lb.transform(test['FinelineNumber'].values.tolist())
-    T_FL.drop(0, axis=1, inplace=True) # 0으로 설정된 컬럼 삭제 (train에 없음)
+    return FN, T_FN
 
-    # df = pd.concat([df, pd.DataFrame(D_FL)], axis=1)
-    # test = pd.concat([test, pd.DataFrame(T_FL)], axis=1)
-
-    return D_FL, T_FL
-
-def DD(df, test):
-    df.dropna(how='all', inplace=True, subset=['DepartmentDescription'])
-
-    a = set(test['DepartmentDescription'].unique())
-    b = set(df['DepartmentDescription'].unique())
-    df = df[df['DepartmentDescription'].isin(a&b)] # train과 test 둘다 있는 것만으로 모델링
+def DD_dummies(df, test):
+    both = set(test['DepartmentDescription'].unique()) & set(df['DepartmentDescription'].unique())
+    df = df[df['DepartmentDescription'].isin(both)] # train과 test 둘다 있는 것만으로 모델링
     df.reset_index(drop=True, inplace=True)
 
-    lb = LabelBinarizer()
-    D_DD = lb.fit_transform(df['DepartmentDescription'].values.tolist())
-    T_DD = lb.transform(test['DepartmentDescription'].values.tolist())
+    DD = pd.get_dummies(df[['VisitNumber','DepartmentDescription']]).groupby('VisitNumber').sum()
+    T_DD = pd.get_dummies(test[['VisitNumber','DepartmentDescription']]).groupby('VisitNumber').sum()
 
-    df = pd.concat([df['VisitNumber'], pd.DataFrame(D_DD)], axis=1)
-    test = pd.concat([test['VisitNumber'], pd.DataFrame(T_DD)], axis=1)
+    not_in_train = list(set(T_DD.columns) - set(DD.columns))
+    if not_in_train:
+        T_DD.drop(not_in_train, axis=1, inplace=True)
 
-    df = df.groupby('VisitNumber').sum()
-    df.reset_index(inplace=True)
-    test = test.groupby('VisitNumber').sum()
-    test.reset_index(inplace=True)
+    DD.reset_index(inplace=True)
+    T_DD.reset_index(inplace=True)
 
-    return df, test
+    return DD, T_DD
 
-
-def editor(df, test):
-    df['weekday'] = df['Weekday'].apply(weekday)
-    test['weekday'] = test['Weekday'].apply(weekday)
-
+def editor(df, test, type='DD'):
     df['refund'] = df['ScanCount'].apply(lambda x: -x if x < 0 else 0)
     df['purchase'] = df['ScanCount'].apply(lambda x: x if x > 0 else 0)
     test['refund'] = test['ScanCount'].apply(lambda x: -x if x < 0 else 0)
     test['purchase'] = test['ScanCount'].apply(lambda x: x if x > 0 else 0)
 
-    df1 = df[['VisitNumber', 'TripType','weekday']].groupby('VisitNumber').mean()
-    df2 = df[['VisitNumber','refund','purchase']].groupby('VisitNumber').sum()
-    test1 = test[['VisitNumber', 'weekday']].groupby('VisitNumber').mean()
-    test2 = test[['VisitNumber','refund','purchase']].groupby('VisitNumber').sum()
+    df1 = df[['VisitNumber','refund','purchase']].groupby('VisitNumber').sum()
+    df2 = pd.get_dummies(df[['VisitNumber','TripType','Weekday']]).groupby('VisitNumber').mean()
+    test1 = test[['VisitNumber','refund','purchase']].groupby('VisitNumber').sum()
+    test2 = pd.get_dummies(test[['VisitNumber', 'Weekday']]).groupby('VisitNumber').mean()
+
     df1.reset_index(inplace=True)
     df2.reset_index(inplace=True)
     test1.reset_index(inplace=True)
@@ -81,22 +57,28 @@ def editor(df, test):
     df1 = pd.merge(df1, df2, on='VisitNumber')
     test1 = pd.merge(test1, test2, on='VisitNumber')
 
-    #bring DepartmentDescription
-    df, test = DD(df, test)
+    #bring dummies
+    if type == 'DD':
+        df, test = DD_dummies(df, test)
+
+    elif type == 'FN':
+        df, test = FN_dummies(df, test)
+
+    elif type == 'all':
+        DD, Dtest = DD_dummies(df, test)
+        FN, Ftest = FN_dummies(df, test)
+        df = pd.merge(DD, FN, on='VisitNumber')
+        test = pd.merge(Dtest, Ftest, on='VisitNumber')
 
     df = pd.merge(df1, df, on='VisitNumber')
     test = pd.merge(test1, test, on='VisitNumber')
-
-    df['weekday'] = df['weekday'].astype('category')  # 카테고리화
-    test['weekday'] = test['weekday'].astype('category')
-
-    # df.dropna(how='any', inplace=True)
-    # df.reset_index(drop=True, inplace=True)
 
     return df, test
 
 df = pd.read_csv("../data/train_v1.csv")
 test = pd.read_csv("test.csv")
-df, test = editor(df, test)
+
+df, test = editor(df, test, type='DD')
+
 df.to_csv('edited_train.csv', index=False)
 test.to_csv('edited_test.csv', index=False)
